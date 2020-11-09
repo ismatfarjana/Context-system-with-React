1c80cf910719af0ed92b275535e84d0eea9066f6
Syeda Ismat Farjana <diptyfmc17@gmail.com>
2020-11-10 04:46:49 +1100

    Components

---
+++ src/components/Field.js
@@ -0,0 +1,15 @@
+// a classbased
+
+import React from "react";
+
+class Field extends React.Component {
+  render() {
+    return (
+      <div className="ui field">
+        <label>Name</label>
+        <input />
+      </div>
+    );
+  }
+}
+export default Field;
