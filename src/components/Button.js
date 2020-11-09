df786189472f9e16f66aeea6516000297c03cfc5
Syeda Ismat Farjana <diptyfmc17@gmail.com>
2020-11-10 06:29:08 +1100

    done for now

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,16 +1,41 @@
 import React from "react";
 import LanguageContext from "../Contexts/LanguageContext";
+import ColorContext from "../Contexts/ColorContext";
 
 class Button extends React.Component {
   //hookup a context object to a class component
-  static contextType = LanguageContext;
+  // //contexttype is only for this.context
+  // static contextType = LanguageContext;
   //static contextType adds a property to our class itself
 
+  // //this.context approach
+  // render() {
+  //   console.log(this.context);
+  //   // setting up text variable for different language
+  //   const text = this.context === "english" ? "submit" : "Voorleggen";
+  //   return <button className="ui button primary">{text}</button>;
+  // }
+
+  //consumer approach
+  renderSubmit(value) {
+    return value === "english" ? "submit" : "voorleggen";
+  }
+  renderButton(color) {
+    return (
+      <button className={`ui button ${color}`}>
+        <LanguageContext.Consumer>
+          {value => this.renderSubmit(value)}
+        </LanguageContext.Consumer>
+      </button>
+    );
+  }
+
   render() {
-    console.log(this.context);
-    // setting up text variable for different language
-    const text = this.context === "english" ? "submit" : "Voorleggen";
-    return <button className="ui button primary">{text}</button>;
+    return (
+      <ColorContext.Consumer>
+        {color => this.renderButton(color)}
+      </ColorContext.Consumer>
+    );
   }
 }
 
