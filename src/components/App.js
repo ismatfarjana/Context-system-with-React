ec25a21fc9fe9987f11b641d9d0f82733866dc5d
Syeda Ismat Farjana <diptyfmc17@gmail.com>
2020-11-10 04:28:57 +1100

    selecting language

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,51 @@
 import React from "react";
 
 class App extends React.Component {
+  state = { language: "bangla" };
+
+  onLanguageChange = language => {
+    this.setState({ language });
+  };
+
   render() {
     return (
       <div className="ui container">
-        <div>APP with CONTEXT</div>
+        <div>
+          Select a language:
+          <i
+            className="flag bd"
+            onClick={() => this.onLanguageChange("bangla")}
+          />
+          <i
+            className="flag us"
+            onClick={() => this.onLanguageChange("english")}
+          />
+          <i
+            className="flag nl"
+            onClick={() => this.onLanguageChange("dutch")}
+          />
+          <i
+            className="flag in"
+            onClick={() => this.onLanguageChange("hindi")}
+          />
+          <i
+            className="flag sa"
+            onClick={() => this.onLanguageChange("arabic")}
+          />
+          <i
+            className="flag ch"
+            onClick={() => this.onLanguageChange("mandarin")}
+          />
+        </div>
+        {this.state.language}
       </div>
     );
   }
 }
 
 export default App;
+
+//seleting language:
+// initalize state obj with initial language
+// add icon with flag
+//next: add clickeventhandler on both flags, make sure anytime user selects either one we  call some callback function that will updat eour currently selected language
